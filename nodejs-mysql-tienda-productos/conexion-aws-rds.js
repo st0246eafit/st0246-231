242fd0c8d4304f014b667c63a858c9da1fdb70a4
edwinm67 <emontoya@eafit.edu.co>
2023-02-16 14:20:38 -0500

    conexion a aws-rds

--- nodejs-mysql-tienda-productos/conexion-aws-rds.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql");
-// Coloca aquí tus credenciales
-module.exports = mysql.createPool({
-  host: "host-aws-rds",
-  user: "dbuser",
-  password: "Eafit2023.",
-  database: "tienda"
-});
--- nodejs-mysql-tienda-productos/conexion.js
+++ nodejs-mysql-tienda-productos/conexion.js
@@ -1,7 +1,10 @@
+// copie el archivo actualice el host con la url adecuada, 
+// si es local: localhost, si es en aws rds, obtenga el host de la base de datos adecuada.
 const mysql = require("mysql");
 // Coloca aquí tus credenciales
 module.exports = mysql.createPool({
   host: "localhost",
+// host: "host-aws-rds",
   user: "dbuser",
   password: "Eafit2023.",
   database: "tienda"
